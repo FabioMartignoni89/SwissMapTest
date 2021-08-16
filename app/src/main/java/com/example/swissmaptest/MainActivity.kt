f9f2aed5115eca6d60442e62741d8a912035ebd3
Fabio Martignoni <fabio.martignoni@dos-group.com>
2021-08-16 11:39:23 +0200

    bump openswissmap SDK dev v.1.1.3.10

--- app/src/main/java/com/example/swissmaptest/MainActivity.kt
+++ app/src/main/java/com/example/swissmaptest/MainActivity.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import ch.admin.geo.openswissmaps.OpenSwissMaps
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        OpenSwissMaps.initialize()
+
         setContentView(R.layout.activity_main)
 
         val openMapButton: Button = this.findViewById(R.id.openMapButton)
